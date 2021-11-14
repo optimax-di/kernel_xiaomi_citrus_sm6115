b1a9e9e3e451f16e4ed3f0d6ecb313d8b1f63500
Muhammad Fadly A S <mhmmdfdlyas@gmail.com>
2021-11-14 13:23:45 +0000

    Revert "[SQUASH]: f2fs: Remove f2fs rapid GC for Android"
    This reverts commit e15c3362b7b9aab65422538413922371b41b3c2b.
    
    Change-Id: I776285a13209495519c4ff87929e4613ad558231

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2780,8 +2780,20 @@ write:
 
 	/* Dentry/quota blocks are controlled by checkpoint */
 	if (S_ISDIR(inode->i_mode) || IS_NOQUOTA(inode)) {
+		/*
+		 * We need to wait for node_write to avoid block allocation during
+		 * checkpoint. This can only happen to quota writes which can cause
+		 * the below discard race condition.
+		 */
+		if (IS_NOQUOTA(inode))
+			down_read(&sbi->node_write);
+
 		fio.need_lock = LOCK_DONE;
 		err = f2fs_do_write_data_page(&fio);
+
+		if (IS_NOQUOTA(inode))
+			up_read(&sbi->node_write);
+
 		goto done;
 	}
 
@@ -3073,8 +3085,11 @@ result:
 					ret = 0;
 					if (wbc->sync_mode == WB_SYNC_ALL) {
 						cond_resched();
-						congestion_wait(BLK_RW_ASYNC,
-							DEFAULT_IO_TIMEOUT);
+#if (CONFIG_HZ > 100)
+						congestion_wait(BLK_RW_ASYNC, 2);
+#else
+						congestion_wait(BLK_RW_ASYNC, 1);
+#endif
 						goto retry_write;
 					}
 					goto next;
